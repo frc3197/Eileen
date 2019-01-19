cdda194c25b6a3be4c05842e820bbbe421cfcb50
Vincent <frozenfire71@gmail.com>
2019-01-19 16:04:55 -0600

    elevator stuff done

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,21 +1,26 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANDigitalInput;
+import com.revrobotics.CANDigitalInput.LimitSwitchPolarity;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.RobotMap;
 import frc.robot.commands.Elevate;
 
 public class Elevator extends Subsystem {
-  private CANSparkMax left = new CANSparkMax(6, MotorType.kBrushless);
-  private CANSparkMax right = new CANSparkMax(5, MotorType.kBrushless);
-  private DigitalInput upperLimitSwitch = new DigitalInput(1);
-  private DigitalInput lowerLimitSwitch = new DigitalInput(2);
+  private CANSparkMax left = new CANSparkMax(RobotMap.CANSparkMaxID.ELEVATORLEFT.id, MotorType.kBrushless);
+  // Bottom limit
+  private CANSparkMax right = new CANSparkMax(RobotMap.CANSparkMaxID.ELEVATORRIGHT.id, MotorType.kBrushless);
+  // Top limit
 
   private SpeedControllerGroup elevatorGroup = new SpeedControllerGroup(left, right);
+
+  private CANDigitalInput bottomLimit = left.getReverseLimitSwitch(LimitSwitchPolarity.kNormallyOpen);
+  private CANDigitalInput topLimit = right.getReverseLimitSwitch(LimitSwitchPolarity.kNormallyOpen);
 
   public Elevator() {
     super();
@@ -27,13 +32,22 @@ public class Elevator extends Subsystem {
     setDefaultCommand(new Elevate());
   }
 
+  /*
+   * Makes sure the elevator can't go any farther up when it hits the
+   * upperLimitSwitch, and that it can't go any farther down when it hits the
+   * lowerLimitSwitch
+   */
   public void drive(double speed) {
-    //if((Math.abs(speed) > .1) ){ Scheduler.getInstance().disable();}
+    SmartDashboard.putBoolean("toplim", topLimit.get());
+    SmartDashboard.putBoolean("bottomlim", bottomLimit.get());
+
     double output = speed;
-      if(upperLimitSwitch.get() == false)
-        output = Math.min(output, 0); //If top pressed(returning a zero value), only drive negative
-      if(lowerLimitSwitch.get() == false)
-        output = Math.max(output, 0);  //If bottom pressed, only drive positive
-      elevatorGroup.set(output);
+    if (topLimit.get()) {
+      output = Math.min(output, 0);
+    } // If top pressed(returning a zero value), only drive negative
+    if (bottomLimit.get()) {
+      output = Math.max(output, 0);
+    } // If bottom pressed, only drive positive
+    elevatorGroup.set(output);
   }
 }
