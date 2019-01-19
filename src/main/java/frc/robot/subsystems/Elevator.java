d233f1411572983217f6fc51fbcea98409b315bd
Vincent <frozenfire71@gmail.com>
2019-01-19 14:03:03 -0600

    encoders - brennan and patrick

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -3,13 +3,17 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.commands.Elevate;
 
 public class Elevator extends Subsystem {
   private CANSparkMax left = new CANSparkMax(6, MotorType.kBrushless);
   private CANSparkMax right = new CANSparkMax(5, MotorType.kBrushless);
+  private DigitalInput upperLimitSwitch = new DigitalInput(1);
+  private DigitalInput lowerLimitSwitch = new DigitalInput(2);
 
   private SpeedControllerGroup elevatorGroup = new SpeedControllerGroup(left, right);
 
@@ -24,6 +28,12 @@ public class Elevator extends Subsystem {
   }
 
   public void drive(double speed) {
-    elevatorGroup.set(speed);
+    //if((Math.abs(speed) > .1) ){ Scheduler.getInstance().disable();}
+    double output = speed;
+      if(upperLimitSwitch.get() == false)
+        output = Math.min(output, 0); //If top pressed(returning a zero value), only drive negative
+      if(lowerLimitSwitch.get() == false)
+        output = Math.max(output, 0);  //If bottom pressed, only drive positive
+      elevatorGroup.set(output);
   }
 }
