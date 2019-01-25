78d4b86bd906b6933565b4e833de3107a4143b90
spencrr <23708360+spencrr@users.noreply.github.com>
2019-01-25 13:38:53 -0600

    visionerror implemented

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,6 +2,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.AlignTurn;
+import frc.robot.commands.ChangeDriveMode;
 
 public class OI {
   public static XboxController joystick = new XboxController(0);
@@ -9,6 +12,10 @@ public class OI {
   public static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 
   public OI() {
-
+    // TODO: real button numbers
+    JoystickButton a = new JoystickButton(joystick, 1);
+    a.whenPressed(new ChangeDriveMode());
+    JoystickButton b = new JoystickButton(joystick, 2);
+    b.whileHeld(new AlignTurn());
   }
 }
--- src/main/java/frc/robot/commands/AlignTurn.java
+++ src/main/java/frc/robot/commands/AlignTurn.java
@@ -1,9 +1,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
+import frc.robot.RobotMap;
 
 public class AlignTurn extends Command {
+
+    private double verticalSpeed;
+    private double turnSpeed;
 
     public AlignTurn() {
         requires(Robot.driveTrain);
@@ -16,7 +21,8 @@ public class AlignTurn extends Command {
 
     @Override
     protected void execute() {
-
+        getContourParameters();
+        Robot.driveTrain.arcadeDrive(verticalSpeed, turnSpeed);
     }
 
     @Override
@@ -26,9 +32,29 @@ public class AlignTurn extends Command {
 
     @Override
     protected void end() {
+        Robot.driveTrain.arcadeDrive(0, 0);
     }
 
-    @Override
-    protected void interrupted() {
+    private void getContourParameters() {
+        double[] contourXs = SmartDashboard.getNumberArray("contour_xs", new double[] {});
+        double[] contourAreas = SmartDashboard.getNumberArray("contour_areas", new double[] {});
+
+        if (contourAreas.length == 2) {
+            double areaError = ((contourAreas[0] + contourAreas[1]) - RobotMap.visionTargetArea);
+            verticalSpeed = -Math.copySign(Math.pow(areaError, 2), areaError);
+            // If totalArea is greater than the target, go backward (-)
+        } else {
+            System.out.println("contourAreas 0");
+            verticalSpeed = 0;
+        }
+
+        if (contourXs.length == 2) {
+            double xError = (((contourXs[0] + contourXs[1]) / 2) - RobotMap.visionTargetX);
+            turnSpeed = -Math.copySign(Math.pow(xError, 2), xError);
+            // If the midX is greater than the target, turn left (-)
+        } else {
+            System.out.println("contourXs 0");
+            turnSpeed = 0;
+        }
     }
 }
