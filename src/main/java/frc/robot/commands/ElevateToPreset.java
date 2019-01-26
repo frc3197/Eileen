c4e72e3fb7fa94a37b050b1c90470c17b72fc7a4
HexHounds <38847793+hexhounds@users.noreply.github.com>
2019-01-26 10:59:17 -0600

    Added SmartDashboard for encoder

--- src/main/java/frc/robot/commands/ElevateToPreset.java
+++ src/main/java/frc/robot/commands/ElevateToPreset.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 import frc.robot.RobotMap.ElevatorPreset;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ElevateToPreset extends Command {
 
@@ -26,6 +27,7 @@ public class ElevateToPreset extends Command {
 
   @Override
   protected void execute() {
+    SmartDashboard.putNumber("Elevator Encoder", Robot.elevator.getEncoderPosition());
     double error = Robot.elevator.getEncoderPosition() - preset.pos;
     finished = Math.abs(error) < RobotMap.elevatorPresetThreshold;
     double speed = -Math.copySign(Math.pow(error, 2), error); // TODO check polarity
