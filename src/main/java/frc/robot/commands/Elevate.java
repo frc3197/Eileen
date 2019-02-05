caf21c2a410e87169bbaac25576dc841a9a75a7e
MINICRISPY <45409781+MINICRISPY@users.noreply.github.com>
2019-02-04 20:59:53 -0600

    added comments to elevate

--- src/main/java/frc/robot/commands/Elevate.java
+++ src/main/java/frc/robot/commands/Elevate.java
@@ -5,11 +5,17 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class Elevate extends Command {
-
+  /**
+   * Constructor calls the elevator object from the robot class
+   */
+    
   public Elevate() {
     requires(Robot.elevator);
   }
-
+  /**
+   * Takes the input from the triggers by subtracting the input of the left from the right
+   * and moves the elevator accordingly
+   */
   @Override
   protected void execute() {
     double speed = Robot.oi.joystick.getTriggerAxis(Hand.kRight) - Robot.oi.joystick.getTriggerAxis(Hand.kLeft);
