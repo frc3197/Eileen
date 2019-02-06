fe60c1499c39260dc4e86587f8c7f638a0a469c9
Spencer <spencers0730@gmail.com>
2019-02-05 22:21:49 -0600

    additional reformatting

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -9,6 +9,8 @@ import frc.robot.commands.AlignTurn;
 import frc.robot.commands.ChangeDriveMode;
 import frc.robot.commands.ElevateToPreset;
 import frc.robot.commands.ElevatorResetPosition;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Elevator;
 
 /**
  * Initializes the joystick and specific buttons
@@ -33,16 +35,16 @@ public class OI {
 
     public static JoystickButton y = new JoystickButton(joystick, 4);
 
-    private static OI oi = new OI();
+    private static OI oi = new OI(Robot.driveTrain, Robot.elevator);
 
-    private OI() {
+    private OI(DriveTrain driveTrain, Elevator elevator) {
         // TODO: real button numbers
 
-        a.whenPressed(new ChangeDriveMode(Robot.driveTrain));
+        a.whenPressed(new ChangeDriveMode(driveTrain));
 
-        b.whileHeld(new AlignTurn(Robot.driveTrain));
+        b.whileHeld(new AlignTurn(driveTrain));
 
-        x.whenPressed(new ElevatorResetPosition(Robot.elevator));
+        x.whenPressed(new ElevatorResetPosition(elevator));
 
         // y.whenPressed(new ElevateToPreset(ElevatorPreset.HATCH_LEVEL_ONE));
         /**
@@ -50,12 +52,12 @@ public class OI {
          * bumper is not held, then the hatch mech will be in position.
          */
         dPadUp.whenPressed(new ElevateToPreset(ElevatorPreset.HATCH_LEVEL_THREE, ElevatorPreset.CARGO_LEVEL_THREE,
-                rightBumper, Robot.elevator));
+                rightBumper, elevator));
         dPadRight.whenPressed(new ElevateToPreset(ElevatorPreset.HATCH_LEVEL_TWO, ElevatorPreset.CARGO_LEVEL_TWO,
-                rightBumper, Robot.elevator));
+                rightBumper, elevator));
         dPadDown.whenPressed(new ElevateToPreset(ElevatorPreset.HATCH_LEVEL_ONE, ElevatorPreset.CARGO_LEVEL_ONE,
-                rightBumper, Robot.elevator));
+                rightBumper, elevator));
         dPadLeft.whenPressed(new ElevateToPreset(ElevatorPreset.CARGO_LOADING_LEVEL, ElevatorPreset.CARGO_SHIP_CARGO,
-                rightBumper, Robot.elevator));
+                rightBumper, elevator));
     }
 }
