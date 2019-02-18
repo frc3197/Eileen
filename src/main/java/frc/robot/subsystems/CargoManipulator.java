c3e1cc35cec6eff86c5cb857825f6fbb5d148ce9
spencrr <23708360+spencrr@users.noreply.github.com>
2019-02-17 21:45:05 -0600

    drivable class and reset abstract

--- src/main/java/frc/robot/subsystems/CargoManipulator.java
+++ src/main/java/frc/robot/subsystems/CargoManipulator.java
@@ -3,10 +3,11 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 import frc.robot.commands.defaults.Manipulate;
 
-public class CargoManipulator extends IntermediateSubystem {
+public class CargoManipulator extends Subsystem implements Drivable {
 
   private CANSparkMax roller = new CANSparkMax(RobotMap.CANSparkMaxID.kCargoManipulator.id, MotorType.kBrushless);
 
