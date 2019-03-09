74e9207b0fe2ac40b12321d0459259ee86c7c75b
spencrr <23708360+spencrr@users.noreply.github.com>
2019-03-08 20:33:44 -0600

    added new climber mech

---
+++ src/main/java/org/team3197/frc2019/robot/subsystems/Climber.java
@@ -0,0 +1,27 @@
+package org.team3197.frc2019.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import org.team3197.frc2019.robot.RobotMap.CANSparkMaxID;
+import org.team3197.frc2019.robot.commands.defaults.Climb;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Climber extends Subsystem {
+  private CANSparkMax vertical = new CANSparkMax(CANSparkMaxID.kLiftVertical.id, MotorType.kBrushless);
+  private CANSparkMax horizontal = new CANSparkMax(CANSparkMaxID.kLiftHorizontal.id, MotorType.kBrushless);
+
+  @Override
+  public void initDefaultCommand() {
+    setDefaultCommand(new Climb(this));
+  }
+
+  public void driveVertical(double speed) {
+    vertical.set(speed);
+  }
+
+  public void driveHorizontal(double speed) {
+    horizontal.set(speed);
+  }
+}
