cfd23258043630ae44a7a8aff20ef8bb7b2407f8
HexHounds <38847793+hexhounds@users.noreply.github.com>
2019-03-15 16:02:10 -0500

    Edited Speeds

--- src/main/java/org/team3197/frc2019/robot/RobotMap.java
+++ src/main/java/org/team3197/frc2019/robot/RobotMap.java
@@ -79,7 +79,7 @@ public class RobotMap {
   };
 
   public static enum GyroSensitivity {
-    kDrive(0.01), kArm(-0.01);
+    kDrive(0.01), kArm(-0.005);
     public double val;
 
     private GyroSensitivity(double val) {
@@ -88,7 +88,7 @@ public class RobotMap {
   };
 
   public static enum MaxSpeeds {
-    kElevator(.7, -.5), kArm(.6), kWrist(.45), kHatch(1), kCargo(1), kClimberVertical(.5), kClimberHorizontal(.75);
+    kElevator(.7, -.5), kArm(.55), kWrist(.30), kHatch(1), kCargo(1), kClimberVertical(.5), kClimberHorizontal(.75);
 
     public double forwardSpeed;
     public double reverseSpeed;
