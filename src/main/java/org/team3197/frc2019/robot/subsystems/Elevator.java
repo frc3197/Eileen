03e7424d0510f702a69fca57fd91722b2266ab28
spencrr <23708360+spencrr@users.noreply.github.com>
2019-03-24 10:03:05 -0500

    removed more junk

--- src/main/java/org/team3197/frc2019/robot/subsystems/Elevator.java
+++ src/main/java/org/team3197/frc2019/robot/subsystems/Elevator.java
@@ -2,7 +2,6 @@ package org.team3197.frc2019.robot.subsystems;
 
 import com.revrobotics.CANDigitalInput;
 import com.revrobotics.CANDigitalInput.LimitSwitchPolarity;
-import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -17,15 +16,12 @@ import org.team3197.frc2019.robot.utilities.FunctionCommand;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.buttons.Trigger;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Elevator extends Subsystem implements Drivable {
   private CANSparkMax left = new CANSparkMax(RobotMap.CANSparkMaxID.kElevatorLeft.id, MotorType.kBrushless);
   private CANSparkMax right = new CANSparkMax(RobotMap.CANSparkMaxID.kElevatorRight.id, MotorType.kBrushless);
 
   private SpeedControllerGroup elevatorGroup = new SpeedControllerGroup(left, right);
-
-  private CANPIDController controller = right.getPIDController();
 
   private CANDigitalInput bottomLimit = left.getReverseLimitSwitch(LimitSwitchPolarity.kNormallyOpen);
   private CANDigitalInput topLimit = right.getReverseLimitSwitch(LimitSwitchPolarity.kNormallyOpen);
@@ -48,41 +44,27 @@ public class Elevator extends Subsystem implements Drivable {
     setDefaultCommand(new Elevate(this));
   }
 
-  private boolean lastDriving = false;
-
-  private double encoderTarget;
-
   public void drive(double speed, boolean hold) {
     // SmartDashboard.putNumber("speed", speed);
     // SmartDashboard.putNumber("ElevatorEncoder", getEncoderPosition());
 
     double output = speed;
-    // if (!bottomLimit.get() && Math.abs(output) < DeadbandType.kElevator.speed) {
-    // output = DeadbandType.kElevator.speed;
-    // }
 
     if (Math.abs(speed) < DeadbandType.kElevator.speed) {
-      controller.setReference(0, ControlType.kSmartMotion);
+      right.getPIDController().setReference(0, ControlType.kSmartMotion);
     } else {
-      right.getEncoder().setPosition(0);
-      controller.setReference(speed, ControlType.kDutyCycle);
+      right.getPIDController().setReference(speed, ControlType.kDutyCycle);
     }
 
     elevatorGroup.set(output);
-
   }
 
-  private double resetEncoderPosition = 0;
-
   public void resetEncoderPosition() {
-    resetEncoderPosition = right.getEncoder().getPosition();
+    right.getEncoder().setPosition(0);
+
   }
 
   public double getEncoderPosition() {
-    return right.getEncoder().getPosition() - resetEncoderPosition;
-  }
-
-  public double getRawEncoderPosition() {
     return right.getEncoder().getPosition();
   }
 
