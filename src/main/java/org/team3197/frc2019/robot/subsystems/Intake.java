599f5a9034fbc7a73bd21c72c215500a7e3feb76
spencrr <23708360+spencrr@users.noreply.github.com>
2019-03-24 09:44:48 -0500

    Create Intake.java

---
+++ src/main/java/org/team3197/frc2019/robot/subsystems/Intake.java
@@ -0,0 +1,31 @@
+package org.team3197.frc2019.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import org.team3197.frc2019.robot.RobotMap;
+import org.team3197.frc2019.robot.commands.defaults.Manipulate;
+import org.team3197.frc2019.robot.utilities.Drivable;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Intake extends Subsystem implements Drivable {
+
+  private CANSparkMax roller = new CANSparkMax(RobotMap.CANSparkMaxID.kIntake.id, MotorType.kBrushless);
+
+  public Intake() {
+    super();
+
+    roller.setIdleMode(IdleMode.kBrake);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    setDefaultCommand(new Manipulate(this));
+  }
+
+  public void drive(double speed, boolean hold) {
+    roller.set(speed);
+  }
+}
