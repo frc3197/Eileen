58158004bb94ba4581d7dd46589bf8e339577610
Spencer <spencers0730@gmail.com>
2019-04-13 10:46:18 -0500

    rename presets

--- src/main/java/org/team3197/frc2019/robot/commands/presets/LevelOne.java
+++ src/main/java/org/team3197/frc2019/robot/commands/presets/LevelOne.java
@@ -2,9 +2,9 @@ package org.team3197.frc2019.robot.commands.presets;
 
 import org.team3197.frc2019.robot.RobotMap.ElbowPreset;
 import org.team3197.frc2019.robot.RobotMap.ElevatorPreset;
-import org.team3197.frc2019.robot.commands.ArticulateToPreset;
+import org.team3197.frc2019.robot.commands.ExtendToPreset;
 import org.team3197.frc2019.robot.commands.ElevateToPreset;
-import org.team3197.frc2019.robot.subsystems.Arm;
+import org.team3197.frc2019.robot.subsystems.Elbow;
 import org.team3197.frc2019.robot.subsystems.Elevator;
 
 import edu.wpi.first.wpilibj.buttons.Trigger;
@@ -12,9 +12,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class LevelOne extends CommandGroup {
 
-  public LevelOne(Elevator elevator, Arm arm, Trigger toggle) {
+  public LevelOne(Elevator elevator, Elbow arm, Trigger toggle) {
 
     addParallel(new ElevateToPreset(ElevatorPreset.kHatchLevelOne, ElevatorPreset.kCargoLevelOne, toggle, elevator));
-    addParallel(new ArticulateToPreset(ElbowPreset.kHatchOne, ElbowPreset.kCargoOne, toggle, arm));
+    addParallel(new ExtendToPreset(ElbowPreset.kHatchOne, ElbowPreset.kCargoOne, toggle, arm));
   }
 }
--- src/main/java/org/team3197/frc2019/robot/commands/presets/LevelTwo.java
+++ src/main/java/org/team3197/frc2019/robot/commands/presets/LevelTwo.java
@@ -2,9 +2,9 @@ package org.team3197.frc2019.robot.commands.presets;
 
 import org.team3197.frc2019.robot.RobotMap.ElbowPreset;
 import org.team3197.frc2019.robot.RobotMap.ElevatorPreset;
-import org.team3197.frc2019.robot.commands.ArticulateToPreset;
+import org.team3197.frc2019.robot.commands.ExtendToPreset;
 import org.team3197.frc2019.robot.commands.ElevateToPreset;
-import org.team3197.frc2019.robot.subsystems.Arm;
+import org.team3197.frc2019.robot.subsystems.Elbow;
 import org.team3197.frc2019.robot.subsystems.Elevator;
 
 import edu.wpi.first.wpilibj.buttons.Trigger;
@@ -12,9 +12,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class LevelTwo extends CommandGroup {
 
-  public LevelTwo(Elevator elevator, Arm arm, Trigger toggle) {
+  public LevelTwo(Elevator elevator, Elbow arm, Trigger toggle) {
 
-    addParallel(new ArticulateToPreset(ElbowPreset.kHatchTwo, ElbowPreset.kCargoTwo, toggle, arm));
+    addParallel(new ExtendToPreset(ElbowPreset.kHatchTwo, ElbowPreset.kCargoTwo, toggle, arm));
     addParallel(new ElevateToPreset(ElevatorPreset.kHatchLevelTwo, ElevatorPreset.kCargoLevelTwo, toggle, elevator));
   }
 }
