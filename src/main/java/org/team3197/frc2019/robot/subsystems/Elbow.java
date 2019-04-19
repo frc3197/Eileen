2c9bbcf74781553d782725e395a426d92ba49547
HexHounds <38847793+hexhounds@users.noreply.github.com>
2019-04-19 15:26:12 -0500

    Tweaks from last saturday we forgot to commit

--- src/main/java/org/team3197/frc2019/robot/subsystems/Elbow.java
+++ src/main/java/org/team3197/frc2019/robot/subsystems/Elbow.java
@@ -28,7 +28,7 @@ public class Elbow extends Subsystem implements Drivable {
 
     elbow.setInverted(false);
 
-    final double kP = 1e-4;
+    final double kP = 2e-4;
     final double kI = 1e-7;
     final double kD = 0;
     final double kIz = 0;
